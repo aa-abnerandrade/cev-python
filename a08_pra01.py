5ae40a5d391263a1638d2a9e4b48282553d56fdd
Abner Andrade <aa.abnerandrade@gmail.com>
2021-11-02 20:33:44 -0300

    aula08

---
+++ a08_pra01.py
@@ -0,0 +1,12 @@
+# AULA 08 - UTILIZANDO MÉTODOS
+
+import random
+from math import sqrt, ceil, trunc
+import emoji
+
+print(emoji.emojize(' :books: AULA 08  :books: \n'))
+
+num = random.randint(1, 25)
+raizquadrada = sqrt(num)
+print(f'A raiz quadrada de {num} é {raizquadrada:.3f}.')
+print(emoji.emojize('Até! :winking_face_with_tongue: \U0001F61C '))
